2a69605e679df75ab66a7540d631ecfabee0a052
Andrew Lindburg <atlindburg@gmail.com>
2018-11-14 21:41:20 +0000

    Forgot to add header for shell

--- shell.h
+++ shell.h
@@ -1,7 +1,20 @@
 #ifndef SHELL
 #define SHELL
+/**
+ * struct builtins - Has builtins and associated funcs
+ * @arg: Builtins name
+ * @builtin: Mathcing builtin func
+ */
+typedef struct builtins
+{
+	char *arg;
+	void (*builtin)(char **args, char *line);
+} builtins_t;
 void shell(void);
 char *_getline(void);
 char **split_line(char *line);
+int check_for_builtins(char **args, char *line);
 int launch_prog(char **args);
+void exit_shell(char **args, char *line);
+int _strcmp(char *s1, char *s2);
 #endif
