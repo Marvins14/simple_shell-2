08c727656dcfcfb0dbadc0a9c5062bd8dac1fc91
Rmolimock <495@holbertonschool.com>
2018-11-17 01:49:00 +0000

    Update: Find path if in path directory, else check if it's in cwd

--- find_path.c
+++ find_path.c
@@ -13,6 +13,8 @@ char* find_path(char *filename)
 	char *path;
 	int len = 0;
 	int i = 0;
+	int execute;
+	char *ret;
 
 	while (filename[len])
 		len++;
@@ -34,15 +36,58 @@ char* find_path(char *filename)
 					break;
 				if (((i + 1) == (len - 1)) && !(sd->d_name[i + 1]))
 				{
-					file_path = strcat(file_path, "/");
-					file_path = strcat(file_path, filename);
-					closedir(dir);
-					return (file_path);
+					ret = strcat(file_path, "/");
+					ret = strcat(ret, filename);
+					execute = access(ret);
+					if (execute == 0)
+					{
+						closedir(dir);
+						return (ret);
+					}
 				}
 			}
 		}
 		closedir(dir);
 		file_path = strtok(NULL, ":");
 	}
-	return ("no\n");
+	ret = check_cwd(filename, len);
+	if (ret != "error")
+		return (ret);
+	return ("error");
+}
+
+char* check_cwd(char *filename, int len)
+{
+	DIR *dir;
+	struct dirent *sd;
+	char *ret;
+	int i = 0;
+	int execute;
+
+	dir = opendir(".");
+	if (!dir)
+	{
+		printf("Error! Unable to open directory.\n");
+		exit(1);
+	}
+	while (sd = readdir(dir))
+	{
+		for (i = 0; sd->d_name[i] && filename[i]; i++)
+		{
+			if (sd->d_name[i] != filename[i])
+				break;
+			if (((i + 1) == (len - 1)) && !(sd->d_name[i + 1]))
+			{
+				ret = "./";
+				ret = strcat(ret, filename);
+				execute = access(ret);
+				if (execute == 0)
+				{
+					closedir(dir);
+					return (ret);
+				}
+			}
+		}
+	}
+	return ("error");
 }
